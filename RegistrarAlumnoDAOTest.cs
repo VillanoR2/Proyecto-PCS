27f1b5e4bc2a8f279bc2bb4ff544c5713906b564
VillanoR2 <47678830+VillanoR2@users.noreply.github.com>
2019-05-07 17:01:37 -0500

    RegistrarInformacion
    Prueba Unitaria de Todos los métodos Registrar

---
+++ RegistrarAlumnoDAOTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LogicaDeNegocio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class PruebaRegistrarInformacion
+
+    {
+        [TestMethod]
+        public void PruebaRegistrarAlumno()
+        {
+            Alumno alumno = new Alumno(); 
+            AlumnoDAO metodo = new AlumnoDAO();
+
+            alumno.Matricula = "S17012946";
+            alumno.Nombre = "Arturo";
+            alumno.Apellido = "Villa Lopez";
+            alumno.CorreoElectronico = "zs17012946@estudiantes.uv.mx";
+            alumno.CarreraAlumno = 0;
+            alumno.ContraseñaAlumno = "Trigoverde2";
+            alumno.FechaNacimiento = "1999-06-19";
+        
+            metodo.registrarAlumno(alumno);
+
+        }
+    }
+}
