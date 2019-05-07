bba217d8dbdc09565eca42f582e6ee709dd68411
ReggieSM <47678904+ReggieSM@users.noreply.github.com>
2019-05-07 16:05:12 -0500

    Interfaces
    Interfaces V.01 para App Servicio social :p

---
+++ ICoordinadorDAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDeNegocio
+{
+    interface ICoordinadorDAO
+    {
+        bool registrarTecnico(TecnicoAcademico tecnico);
+
+        bool registrarProyeto(Proyecto proyecto);
+
+        bool registrarInstitucion(Institucion institucion);
+
+        bool registrarEncargado(Encargado encargado);
+
+        bool asignarFechaADocumento(Documento documento);
+
+    }
+}
---
+++ IProyectoDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaDeNegocio
+{
+    interface IProyectoDAO
+    {
+        void asignarAlumno(Alumno Alumno);
+
+        void calcularVacanates();
+
+        void cambiarEstadoProyecto(Proyecto EstadoProyecto);
+
+        void darBajaAlumno(Alumno alumno);
+
+        void calcularHoras(Proyecto Horas);
+    }
+}
