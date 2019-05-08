595829da307343ce6bdeb6f18711c6640df9da8c
ReggieSM <47678904+ReggieSM@users.noreply.github.com>
2019-05-07 22:05:50 -0500

    Update RegistrarAlumnoDAOTest.cs

--- RegistrarAlumnoDAOTest.cs
+++ RegistrarAlumnoDAOTest.cs
@@ -29,5 +29,47 @@ namespace UnitTest
             Assert.AreEqual(resultadoEsperado, resultadoObtenido, "Prueba agregar estudiante válido");
 
         }
+        
+        public void PruebaRegistrarAlumnoNoValido()
+        {
+           Alumno alumno = new Alumno(); 
+            AlumnoDAO metodo = new AlumnoDAO();
+            bool resultadoEsperado = true;
+            bool resultadoObtenido = false;
+
+            alumno.Matricula = "S17012900000";
+            alumno.Nombre = "Regina";
+            alumno.Apellido = "Saavedra";
+            alumno.CorreoElectronico = "zs17012923@estudiantes.uv.mx";
+            alumno.CarreraAlumno = 0;
+            alumno.ContraseñaAlumno = "calitom";
+            alumno.FechaNacimiento = "1998-11-08";
+        
+            resultadoObtenido=metodo.registrarAlumno(alumno);
+
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido, "Matricula excede los valores permitidos");
+
+        }
+        
+        public void PruebaRegistrarAlumnoLlavePrimaria()
+        {
+           Alumno alumno = new Alumno(); 
+            AlumnoDAO metodo = new AlumnoDAO();
+            bool resultadoEsperado = true;
+            bool resultadoObtenido = false;
+
+            alumno.Matricula = "S17012946";
+            alumno.Nombre = "Juan";
+            alumno.Apellido = "Hernandez";
+            alumno.CorreoElectronico = "zs17012943@estudiantes.uv.mx";
+            alumno.CarreraAlumno = 0;
+            alumno.ContraseñaAlumno = "juan123";
+            alumno.FechaNacimiento = "1998-10-09";
+        
+            resultadoObtenido=metodo.registrarAlumno(alumno);
+
+            Assert.AreEqual(resultadoEsperado, resultadoObtenido, "Llave primaria repetida");
+
+        }
     }
 }
